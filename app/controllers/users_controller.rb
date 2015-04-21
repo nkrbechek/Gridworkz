c650ef7ba8cb1baa29268ee53dd4582841d93b56
nkrbechek <nkrbechek@yahoo.com>
2015-04-21 14:44:41 -0700

    login added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   	def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      flash[:success] = "Welcome to Gridworkz!"
       redirect_to @user# Handle a successful save.
     else
       render 'new'
