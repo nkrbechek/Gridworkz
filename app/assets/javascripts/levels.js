d6f5312a508f8549e9d3adf8a46196ae983335f3
Paz Zait-Givon <paz@umail.ucsb.edu>
2015-04-30 09:23:06 -0700

    all nosepperation generators, and refactoring

--- app/assets/javascripts/levels.js
+++ app/assets/javascripts/levels.js
@@ -13,7 +13,7 @@ function block(gridsize)
 	var rowNumber = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
 	for (var i = 0; i <gridsize; i++) 
 	{
-		result[i]=new Array()
+		result[i]=new Array();
 		for (var j = 0; j <gridsize; j++) 
 		{  		
 			if(row==1)//row not column
@@ -35,22 +35,27 @@ function block(gridsize)
 	return result;
 }
 
-function sides(gridsize)//not the most efficiant
-{	var result = new Array();
-	var filledSquares=Math.floor(gridsize*gridsize/3)-1;//30% rounded down -first point
-	var starti = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
-	var startj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
-	//initializing all to zero
+function initialize(gridsize)
+{	
+	var result = new Array();
 	for (var i = 0; i <gridsize; i++) 
 	{
-		result[i]=new Array()
+		result[i]=new Array();
 		for (var j = 0; j <gridsize; j++) 
 		{  		
 			result[i][j]=0; 
 		}//end inner for loop
 	}//end outer for loop
-	//fillin squares
-	while(filledSquares!=0)
+	return result;
+}
+
+function sides(gridsize)//not the most efficiant
+{	var result = initialize(gridsize);
+	var filledSquares=Math.floor(gridsize*gridsize/3)-1;//30% rounded down -first point
+	var starti = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	var startj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+		result[starti][startj]=1;
+	while(filledSquares!==0)	//fillin squares
 	{
 		var direction=Math.floor(Math.random()*4);
 		//0->i--, 1->i++, 2->j--, 3->j++;
@@ -59,35 +64,120 @@ function sides(gridsize)//not the most efficiant
 		switch(direction)
 		{
 			case 0:
-			 	tempi=starti--;
+			 	tempi=starti-1;
 			 	break;
 			case 1:
-				tempi=starti++;
+				tempi=starti+1;
 				break;
 			case 2:
-				tempj--;
+				tempj=startj-1;
 				break;
 			case 3:
-				tempj++;
+				tempj=startj+1;
+				break;
+			default:
+				console.log('something went wrong');
 				break;
 		}//end switch case
-		if(tempi<gridsize && tempj<gridsize && tempi>=0 && tempj>=0 && result[i][j]==0 )
+		if(tempi<gridsize && tempj<gridsize && tempi>=0 && tempj>=0 && result[i][j]===0 )
 		{
-			result[i][j]=1
+			result[i][j]=1;
 			filledSquares--;
 		}
-	}
+	}//end loop
 	return result;
 }
 
 function corners(gridsize)
 {
-	
+	var result = initialize(gridsize);
+	var filledSquares=Math.floor(gridsize*gridsize/3)-1;//30% rounded down -first point
+	var starti = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	var startj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+		result[starti][startj]=1;
+	while(filledSquares!==0)	//fillin squares
+	{
+		var direction=Math.floor(Math.random()*4);
+		//0->i--, 1->i++, 2->j--, 3->j++;
+		var tempi;
+		var tempj;
+		switch(direction)
+		{
+			case 0:
+			 	tempi=starti-1; tempj=startj-1;
+			 	break;
+			case 1:
+				tempi=starti+1; tempj=startj+1;
+				break;
+			case 2:
+				tempj=startj-1; tempi=starti+1;
+				break;
+			case 3:
+				tempj=startj+1; tempi=starti-1;
+				break;
+			default:
+				console.log('something went wrong');
+				break;
+		}//end switch case
+		if(tempi<gridsize && tempj<gridsize && tempi>=0 && tempj>=0 && result[i][j]===0 )
+		{
+			result[i][j]=1;
+			filledSquares--;
+		}
+	}//end loop
+	return result;	
 }
 
 function cornerSides(gridsize)
-{
-	
+{	
+	var result = initialize(gridsize);
+	var filledSquares=Math.floor(gridsize*gridsize/3)-1;//30% rounded down -first point
+	var starti = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	var startj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+		result[starti][startj]=1;
+	while(filledSquares!==0)	//fillin squares
+	{
+		var direction=Math.floor(Math.random()*8);
+		//0->i--, 1->i++, 2->j--, 3->j++;
+		var tempi;
+		var tempj;
+		switch(direction)
+		{
+			case 0:
+			 	tempi=starti-1; tempj=startj-1;
+			 	break;
+			case 1:
+				tempi=starti+1; tempj=startj+1;
+				break;
+			case 2:
+				tempj=startj-1; tempi=starti+1;
+				break;
+			case 3:
+				tempj=startj+1; tempi=starti-1;
+				break;
+			case 4:
+			 	tempi=starti-1;
+			 	break;
+			case 5:
+				tempi=starti+1;
+				break;
+			case 6:
+				tempj=startj-1; 
+				break;
+			case 7:
+				tempj=startj+1;
+				break;
+			default:
+				console.log('something went wrong');
+				break;
+		}//end switch case
+		if(tempi<gridsize && tempj<gridsize && tempi>=0 && tempj>=0 && result[i][j]===0 )
+		{
+			result[i][j]=1;
+			filledSquares--;
+		}
+	}//end loop
+	return result;
 }
 
 function sepSides(gridsize)
