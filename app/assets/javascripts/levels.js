7436e30515ed1f7421341ec19de5f22cfb48c8a7
Paz Zait-Givon <PazZaitGivon@users.noreply.github.com>
2015-05-03 17:19:17 -0700

    added first sepperated

--- app/assets/javascripts/levels.js
+++ app/assets/javascripts/levels.js
@@ -180,9 +180,118 @@ function cornerSides(gridsize)
 	return result;
 }
 
-function sepSides(gridsize)
+function doNotTouch(ai, aj, bi, bj)
 {
-	var result = initialize(gridsize);	
+	if(ai===bi && (aj===bj || aj+1===bj || aj-1===bj) )
+	return false;
+	if(ai===bi+1 && (aj===bj || aj+1===bj || aj-1===bj) )
+	return false;
+	if(ai===bi-1 && (aj===bj || aj+1===bj || aj-1===bj) )
+	return false;
+	
+	return true;
+}
+
+function doesNotTouchGroup(ai,aj,array,gridsize)
+{
+	var result=true;
+	if(array[ai][aj]==1 || array[ai][aj+1] || array[ai][aj-1])
+		return false;
+	if(array[ai+1][aj]==1 || array[ai+1][aj+1] || array[ai+1][aj-1])	
+		return false;
+	if(array[ai-1][aj]==1 || array[ai-1][aj+1] || array[ai-1][aj-1])	
+		return false;
+	
+	
+	return true;
+}
+
+function mergeArs(array1, array2, gridsize)
+{
+	var result=new Array();
+	for(var i=0; i<gridsize; i++)
+	{
+		for(var j=0; j<gridsize; j++)
+		{
+			if(array1[i][j]===1 || array2[i][j]===1)
+				{result[i][j]=1;}
+			else
+				{result=0;}
+		}
+	}
+	
+	return result;
+}
+
+function sepSides(gridsize)//Ill make two islands
+{
+	var resulta=initialize(gridsize);
+	var resultb=initialize(gridsize);
+
+	var filledSquares=Math.floor(gridsize*gridsize/3)-1;//30% rounded down -first point
+	
+	var ai = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	var aj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+		result[ai][aj]='a';
+	var bi = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	var bj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	
+	//making sure sepperate start pts
+	while(doNotTouch(ai,aj,bi,bj)!==true)
+	{
+		bi = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+		bj = Math.floor(Math.random()*gridsize);//0-(gridsize-1)
+	}
+	
+	while(filledSquares>0)
+	{
+		var direction=Math.floor(Math.random()*4);
+		//0->i--, 1->i++, 2->j--, 3->j++;
+		var tempi;
+		var tempj;
+		switch(direction)
+		{
+			case 0:
+				if(filledSquares%2===0)
+			 		tempi=ai-1;
+				else
+					tempi=bi-1;
+			 	break;
+			case 1:
+				if(filledSquares%2===0)
+					tempi=aj+1;
+				else
+					tempi=bj+1;
+				break;
+			case 2:
+				if(filledSquares%2===0)
+					tempj=aj-1;
+				else
+					tempj=bj-1;
+				break;
+			case 3:
+				if(filledSquares%2===0)
+					tempj=aj+1;
+				else
+					tempj=bj+1;
+					
+				break;
+			default:
+				console.log('something went wrong');
+				break;
+		}//end switch case
+		if(filledSquares%2===0 && doesNotTouchGroup(tempi,tempj,resultb) )//we came from a
+		{
+			resulta[i][j]=1;
+			filledSquares--;
+		}
+		   if(filledSquares%2!==0 && doesNotTouchGroup(tempi,tempj,resulta) )//we came from b
+		{
+			resultb[i][j]=1;
+			filledSquares--;
+		}
+	}
+	return mergeArs(resulta,resultb,gridsize);
 }
 
 function sepCorners(gridsize)
@@ -194,6 +303,7 @@ function sepMixed(gridsize)
 {
 	var result = initialize(gridsize);
 }
+
 
 function islands(gridsize)
 {
