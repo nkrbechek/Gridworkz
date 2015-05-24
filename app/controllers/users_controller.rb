9c2a14718f0b847f08aa4730c5c9c6b6f79fd88b
nkrbechek <nkrbechek@yahoo.com>
2015-05-23 17:59:36 -0700

    getting the controller functions ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def game 
-    @user = User.find(session[:user_id]) 
+    @user = User.find(session[:user_id])
+    def add_info
+    add_info_with_para(params)
   end
+  end
+  
+ 
   def new
   	@user = User.new
     def create
@@ -28,5 +33,17 @@ end
       params.require(:user).permit(:name, :age, :gender, :password,
                                    :password_confirmation)
     end
- 
+  
+    def add_info_with_para p = {}
+      user = User.find(session[:user_id])
+      user.speedup = p[:speed]
+      user.corrRow = p[:correctRow]
+      user.gridSize = p[:grid]
+      user.levelNum = p[:level]
+      user.levelType = p[:type]
+      user.attempts = p[:total]
+      user.levelData = p[:levelInfo]
+      user.totalCorrect = p[:correct] 
+    end
+
 end
